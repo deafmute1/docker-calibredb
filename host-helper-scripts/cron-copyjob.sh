81e421a5d16bc150b4738f53d70c4d9fdeb0e1f2
deafmute <me@ethandjeric.com>
2020-11-10 17:00:29 +1100

    strips path using basename in cron-copyjob.sh where necessary

--- host-helper-scripts/cron-copyjob.sh
+++ host-helper-scripts/cron-copyjob.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# Takes a root download path ($1) and looks for new items to copy in first level subdirectories
+# Takes a root source path ($1) and looks for new items to copy in first level subdirectories
 # These files are copied to the desination path ($2), maintaining the first level subdirectory
-# $3 specifies how old items to be included are (a integer, representing minutes). Match this to a cron job.
+# $3 specifies how old items to be included are in minutes (as an integer). Match this to a cron job.
 
-jobTimer=$(( ("$3" + 1) * -1 ))
+# use a negative value for `find -mmin` to find files newer then the positive value.
+# add +5 for safety - missing a file will result in it never being imported, but copying twice will just waste a little time in entrypoint.sh
+jobTimer=$(( ("$3" + 5) * -1 ))
 
 # parsing `find` into `for` is bad apparently, use while read instead
-# on find -mmin: use a negative value to find files newer then the positive value.
-while read -r subDir; do
-    if [[ ! -f "${2}/${subDir}" ]]; then 
-        mkdir -p "${2}/${subDir}"
+while read -r subPath; do
+    subFolder=$(basename "$subPath")
+    if [[ ! -f "${2}/${subFolder}" ]]; then 
+        mkdir -p "${2}/${subFolder}"
     fi
-    find "$subDir" -type f  -mmin "$jobTimer" -exec rsync -a --ignore-existing {} "${2}/${subDir}" \;
+    find "$subPath" -type f -mmin "$jobTimer" -exec cp '{}' "${2}/${subFolder}/" \;
 done < <(find "$1" -mindepth 1 -maxdepth 1 -type d)
